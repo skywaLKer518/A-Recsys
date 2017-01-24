c206b4e7171e0119527d31a8d20e0dca71d47055
Kuan Liu <liukuan0518@gmail.com>
2017-01-23 21:21:53 -0800

    fix bug for evaluation
    when evaluate on partial dataset

--- utils/xing_eval.py
+++ utils/xing_eval.py
@@ -1,17 +1,26 @@
 from xing_submit import *
 from load_xing_data import load_user_target_csv, load_item_active_csv, load_user_csv
+from os.path import join
+import cPickle as pickle
+from pandatools import build_index
 
 class Evaluate(object):
     def __init__(self, logit_ind2item_ind, res_filename='../submissions/res_T.csv', 
-        hist_filename = '../submissions/historical_train.csv', ta=1, old=None):
+        hist_filename = '../submissions/historical_train.csv', ta=1, old=None, data_dir=None):
         self.logit_ind2item_ind = logit_ind2item_ind
         self.T = load_submit(res_filename)
         self.hist = load_submit(hist_filename)
         self.Iatt, _, self.Iid2ind = load_item_active_csv()
-        if ta == 1:
-            self.Uatt, _, self.Uid2ind = load_user_target_csv()
+
+        if data_dir is not None:
+            filename = 'processed_user' + '_ta_' + str(ta)
+            self.Uatt = pickle.load(open(join(data_dir, filename), 'rb'))
+            self.Uid2ind = build_index(self.Uatt)
         else:
-            self.Uatt, _, self.Uid2ind = load_user_csv()
+            if ta == 1:
+                self.Uatt, _, self.Uid2ind = load_user_target_csv()
+            else:
+                self.Uatt, _, self.Uid2ind = load_user_csv()
 
         self.Uids = self.get_uids()
         self.Uinds = [self.Uid2ind[v] for v in self.Uids]
